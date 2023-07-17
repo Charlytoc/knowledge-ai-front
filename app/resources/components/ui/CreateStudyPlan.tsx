dd682be8ab58c9a7616acab659c9e093ea21ef5a
Charly Chacón <107764250+Charlytoc@users.noreply.github.com>
2023-07-16 22:35:41 -0500

    Actualizar CreateStudyPlan.tsx

--- app/resources/components/ui/CreateStudyPlan.tsx
+++ app/resources/components/ui/CreateStudyPlan.tsx
@@ -36,6 +36,9 @@ export default function StudyPlanCreate() {
     } else {
       console.error("Element is not a form or does not exist");
     }
+
+  setShowForm((prevState) => !prevState);
+  };
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
